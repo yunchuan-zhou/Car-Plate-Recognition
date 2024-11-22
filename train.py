552f446aab6b552f087fbd93fc07e1ebc9370bb4
yunchuan-zhou <yunchuan.zhou@mail.utoronto.ca>
2024-11-21 20:10:28 -0500

    improve data processing method

--- train.py
+++ train.py
@@ -4,6 +4,8 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 from PIL import Image
 import matplotlib.pyplot as plt
+import numpy as np
+import cv2
 
 
 class LicensePlateDataset(Dataset):
@@ -17,22 +19,44 @@ class LicensePlateDataset(Dataset):
     def __getitem__(self, idx):
         img_path = self.data.iloc[idx, 1]  
         label = self.data.iloc[idx, 2]    
-        image = Image.open(img_path).convert("RGB")
+        image = Image.open(img_path).convert("L")
 
         if self.transform:
             image = self.transform(image)
 
         return image, label
     
+class ResizeWithPadding:
+    def __init__(self, target_height=32, target_width=128):
+        self.target_height = target_height
+        self.target_width = target_width
 
+    def __call__(self, image):
+        image = np.array(image)
+        h, w = image.shape[:2]
+
+
+        scale = self.target_height / h
+        new_width = int(w * scale)
+        resized_image = cv2.resize(image, (new_width, self.target_height))
+
+
+        padded_image = np.zeros((self.target_height, self.target_width), dtype=np.uint8)
+        padded_image[:, :min(new_width, self.target_width)] = resized_image[:, :min(new_width, self.target_width)]
+
+
+        return Image.fromarray(padded_image)
+    
 if __name__ == '__main__':
 
 
     # normalize the images
     transform = transforms.Compose([
-        transforms.Resize((128, 128)),
-        transforms.ToTensor(),        
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) 
+        ResizeWithPadding(target_height=32, target_width=128),
+        #transforms.RandomRotation(5),  
+        #transforms.ColorJitter(brightness=0.2, contrast=0.2),  
+        transforms.ToTensor(),  
+        transforms.Normalize(mean=[0.5], std=[0.5])  
     ])
     dataset = LicensePlateDataset(csv_file='labels_test.csv', transform=transform)
 
@@ -44,9 +68,9 @@ if __name__ == '__main__':
     images, labels = next(iter(dataloader))
     fig, axes = plt.subplots(1, 5, figsize=(15, 5))
     for i in range(5):
-        image = images[i].permute(1, 2, 0).numpy()  # convert tensor (C, H, W) to (H, W, C)
-        image = (image * 0.5) + 0.5  # denormalize
-        axes[i].imshow(image)
+        image = images[i].permute(1, 2, 0).numpy()  
+        image = (image * 0.5) + 0.5  
+        axes[i].imshow(image.squeeze(), cmap='gray')  
         axes[i].set_title(f"Label: {labels[i]}")
         axes[i].axis("off")
     plt.show()
